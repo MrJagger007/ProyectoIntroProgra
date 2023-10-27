57874740c2cd7e01cdd3951ffbbb105ca6f67c44
Ronny López <134113836+MrJagger007@users.noreply.github.com>
2023-10-27 14:18:51 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package proyectointroprogra;
+import javax.swing.JOptionPane;
+public class Main {
+
+
+    public static void main(String[] args) {
+       //SucursalesFixTech Sucursal=new SucursalesFixTech();   
+       //UsuariosFixTech Usuario=new UsuariosFixTech();      
+       //Facturacion Factura=new Facturacion();
+       ClientesFixTech Cliente=new ClientesFixTech();
+       //Sucursal.FixTechInformation();
+       //Usuario.UsuariosFixTech();
+       //Factura.Facturacion();
+       Cliente.ClientesFixTexch();
+        
+    }
+    
+}
