374e9c68ae7d7d2087f469281446790c65d68b19
Yoilin Mejía <119643387+Itsyoiyo@users.noreply.github.com>
2023-11-16 09:44:15 -0600

    CatalogoCategoriasSettersyGetters

--- CatalogoCategoriasEquipos.java
+++ CatalogoCategoriasEquipos.java
@@ -1,25 +1,50 @@
 package proyectointroprogra;
 
+
 import javax.swing.JOptionPane;
 
 public class CatalogoCategoriasEquipos {
 
-    private String nameBranch;
-    private String cityBranch;
-    private String branchAdress;
-    private String branchTelephone;
-    private String email;
-    private String branchStatus;
+    private String categoryName;
+    private String categoryFealure;
+    private String hardwareStatus;
+
 
     public CatalogoCategoriasEquipos() {
 
-        this.nameBranch = "";
-        this.cityBranch = "";
-        this.branchAdress = "";
-        this.branchTelephone = "";
-        this.email = "";
-        this.branchStatus = "";
+        this.categoryName = "";
+        this.categoryFealure = "";
+        this.hardwareStatus = "";
 
     }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+   
+    public void setCategoryFeature(String categoryFeature) {
+        this.categoryFeature = categoryFeature;
+    }
+
+
+    public void setHardwareStatus(String hardwareStatus) {
+        this.hardwareStatus = hardwareStatus;
+    }
+
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+
+    public String getCategoryFeature() {
+        return categoryFeature;
+    }
+
+    public String getHardwareStatus() {
+        return hardwareStatus;
+    }
+  
+
 
 }
